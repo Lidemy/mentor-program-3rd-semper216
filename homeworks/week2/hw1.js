6fab1cbfdf663ddf5348479982afe38ab042dec4
semper216 <sempertarjousamcus@gmail.com>
2019-05-07 23:59:30 +0800

    finished hw1 and hw2

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,7 @@
 function printStars(n) {
-  console.log(n);
+	for (var i=1;i<=n;i++) {
+		console.log('*')
+	}
 }
 
-printStars(5);
+printStars(6);
--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,13 @@
 function capitalize(str) {
-  return str;
+	if (97<=str[0].charCodeAt()<=122 ){
+		first_word = str[0]
+		turn_UpperCase = first_word.toUpperCase()
+		new_string = str.replace(/^[a-z]/,turn_UpperCase)
+		console.log(new_string)
+    return new_stringi
+    }
 }
+
+capitalize('hello')
 
 console.log(capitalize('hello'));
