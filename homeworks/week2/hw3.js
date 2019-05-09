f0f6fd77b7739dc897234211cf0cb2313a304fdc
semper216 <sempertarjousamcus@gmail.com>
2019-05-10 01:39:15 +0800

    delete needless line

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -2,7 +2,6 @@ function reverse(str) {
 	str_len = str.length
     new_order= []
     for(i=0;i<str_len;i++){
-    	old_obj = str[i]
 	    new_obj = str[str_len-1-i]
 	    new_order.push(new_obj)
 	    new_str= new_order.join('')
