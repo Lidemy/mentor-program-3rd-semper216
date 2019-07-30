9357248fb3c54d09310380348dfa3148f7336580
unknown <sempertarjousamicus@going.iis.sinica.edu.tw>
2019-07-31 01:12:49 +0800

    finished hw

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,23 @@
 function alphaSwap(str) {
-  console.log(str);
+  const strArr = str.split('');
+  const NewWord = [];
+  const Strlen = str.length;
+  for (let w = 0; w < Strlen; w += 1) {
+    const asciiW = strArr[w].charCodeAt(0);
+    if (asciiW >= 97 && asciiW <= 122) {
+      const Wnew = strArr[w].toUpperCase();
+      NewWord.push(Wnew);
+    }
+    if (asciiW >= 65 && asciiW <= 90) {
+      const Wnew = strArr[w].toLowerCase();
+      NewWord.push(Wnew);
+    }
+    const NotChar = [91, 91, 92, 92, 94, 95, 96];
+    if (asciiW > 122 || asciiW < 65 || asciiW in NotChar) {
+      NewWord.push(w);
+    }
+  }
+  const newStr = NewWord.join('');
+  return newStr;
 }
-
 module.exports = alphaSwap;
--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,18 @@
 function isPalindromes(str) {
-  console.log(str);
+  const inWord = str.split('');
+  const outWord = [];
+  const n = inWord.length;
+  for (let i = 0; i <= n; i += 1) {
+    outWord.push(inWord[n - i]);
+  }
+  let result;
+  if (inWord.join('') === outWord.join('')) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
 }
 
 module.exports = isPalindromes;
+isPalindromes('applppa');
