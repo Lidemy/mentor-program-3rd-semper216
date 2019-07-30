a02c34aa9f7e2ea26b413935948a9bcbea55ec8e
unknown <sempertarjousamicus@going.iis.sinica.edu.tw>
2019-07-31 02:23:09 +0800

    week2 hw5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,25 @@
-function join(str, concatStr) {
-  return concatStr;
+function join(arr, concatStr) {
+  let newStr = '';
+  if (arr.length === 0) {
+    newStr = '';
+  }
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    newStr = newStr + arr[i] + concatStr;
+  }
+  const lastStr = arr[arr.length - 1];
+  newStr += lastStr;
+  return newStr;
 }
+
+const arr = ['a', 'b', 'c'];
+console.log(join(arr, '!'));
 
 function repeat(str, times) {
-  return times;
+  let reString = str;
+  for (let i = 1; i <= times; i += 1) {
+    reString += str;
+  }
+  return reString;
 }
 
-console.log(join('a', '!'));
-console.log(repeat('a', 5));
+repeat('a', 5);
