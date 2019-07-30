6b39ab3ae63dc83c1aaca773fa014135bb84ebb8
unknown <sempertarjousamicus@going.iis.sinica.edu.tw>
2019-07-31 02:02:18 +0800

    week2 hw3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,15 @@
 function reverse(str) {
-  return str;
+  const strLen = str.length;
+  const newOrder = [];
+  let newObj = '';
+  let newStr = '';
+  for (let i = 0; i < strLen; i += 1) {
+    newObj = str[strLen - 1 - i];
+    newOrder.push(newObj);
+    newStr = newOrder.join('');
+  }
+  console.log(newStr);
+  return newStr;
 }
 
 reverse('hello');
